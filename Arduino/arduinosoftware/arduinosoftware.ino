7dc460fff1773b06d1cde716d003e57fb369bdd8
Drew Patel <drew1patel@gmail.com>
2018-10-29 16:31:02 -0500

    added arm function

--- Arduino/arduinosoftware/arduinosoftware.ino
+++ Arduino/arduinosoftware/arduinosoftware.ino
@@ -10,14 +10,29 @@ void setup()
 {
   Serial.begin(9600);
   esc.attach(9);
+  arm();
 }
- 
+
+void arm(){
+ // arm the speed controller, modify as necessary for your ESC  
+ setSpeed(0); 
+ delay(1000); //delay 1 second,  some speed controllers may need longer
+}
+
+void setSpeed(int speed){
+ // speed is from 0 to 100 where 0 is off and 100 is maximum speed
+ //the following maps speed values of 0-100 to angles from 0-180,
+ // some speed controllers may need different values, see the ESC instructions
+ int angle = map(speed, 0, 100, 0, 180);
+ esc.write(angle);    
+}
+
 void loop()
 {
  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 660, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   Serial.println(val);                  // sets the servo position according to the scaled value
-  esc.write(val);      
+  setSpeed(val);      
 
   /*
   if (Serial.available() > 0) {

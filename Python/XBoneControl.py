9f65ae262b08677e976a6d3d14c64d8f5e452360
Chase Blackburrn <chaseblackburn01@gmail.com>
2019-02-18 16:58:49 -0600

    Added functions in controller script
    Calls movement functions

--- Python/XBoneControl.py
+++ Python/XBoneControl.py
@@ -29,3 +29,38 @@ while True:
                     print("11")
                 if(i == 4):
                     pygame.quit()
+
+
+        # i = axis number
+        # axes = value of the axis(i) 
+        for i in range( axes ):
+            
+            axis = joystick.get_axis( i )
+            
+            if (axis > -0.1 and axis < 0.1):
+                axis = 0
+                    if (i > 3):
+                        axis+=1
+                        axis = axis/2
+                if (abs(axis) > 0):
+                    
+                    #Changes axis values from -1 to 1 into 0-100
+                    axis = (axis + 1) * 50
+                    
+                    #Checking input from diffent axis
+                    if (i == 0):
+                        if (axis < 0):
+                            doMovementLeft(axis)
+                        elif (axis > 0):
+                            doMovementRight(axis)
+                    if (i == 1):
+                        if (axis < 0):
+                            doMovementForward(axis)
+                        elif (axis > 0):
+                            doMovementBackward(axis)
+
+
+
+
+
+

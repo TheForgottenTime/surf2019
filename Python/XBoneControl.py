6eab57636b4901f433b25ca86700eca50a30c823
Chase Blackburrn <chaseblackburn01@gmail.com>
2019-02-20 16:39:05 -0600

    Update XBoneControl.py

--- Python/XBoneControl.py
+++ Python/XBoneControl.py
@@ -1,4 +1,5 @@
 import pygame
+from arduinoSerialCommunicator import writeValue
 
 pygame.init()
 pygame.joystick.init()
@@ -25,14 +26,18 @@ while True:
             if(button):
                 if(i == 12):
                     print("12")
+                    writeValue("0,90,0")
                 if(i == 11):
                     print("11")
+                    writeValue("0,20,1")
                 if(i == 4):
                     pygame.quit()
 
 
         # i = axis number
         # axes = value of the axis(i)
+        axes = joystick.get_numaxes()
+
         for i in range( axes ):
             
             axis = joystick.get_axis( i )
@@ -42,22 +47,37 @@ while True:
                 if (i > 3):
                     axis+=1
                     axis = axis/2
-                if (abs(axis) > 0):
-                    
-                    #Changes axis values from -1 to 1 into 0-100
-                    axis = (axis + 1) * 50
-                    
-                    #Checking input from diffent axis
-                    if (i == 0):
-                        if (axis < 0):
-                            doMovementLeft(axis)
-                        elif (axis > 0):
-                            doMovementRight(axis)
-                    if (i == 1):
-                        if (axis < 0):
-                            doMovementForward(axis)
-                        elif (axis > 0):
-                            doMovementBackward(axis)
+            if (abs(axis) > 0):
+                
+                #Changes axis values from -1 to 1 into 0-100
+                axis = (axis + 1) * 50
+                
+                
+                
+                #Checking input from diffent axis
+                if (i == 0):
+                    if (axis < 50):
+                        #doMovementLeft(axis)
+                        print("left: ")
+                        print(axis)
+                    elif (axis > 50):
+                        #doMovementRight(axis)
+                        print("right: ")
+                        print(axis)
+                if (i == 1):
+                    if (axis < 50):
+                        #doMovementForward(axis)
+                        print("forward: ")
+                        print(axis)
+                        passingString = "0," + axis + ",0"
+                        writeValue(passingString)
+                    elif (axis > 50):
+                        #doMovementBackward(axis)
+                        print("backward: ")
+                        print(axis)
+                        passingString = "0," + axis + ",1"
+                        writeValue(passingString)
+
 
 
 

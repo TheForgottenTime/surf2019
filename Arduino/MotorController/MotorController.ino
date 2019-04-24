b7dbae6321d5cb82c600ccc9a9863bc486ce6589
Drew Patel <drew1patel@gmail.com>
2019-04-24 16:49:47 -0500

    changed input style

--- Arduino/MotorController/MotorController.ino
+++ Arduino/MotorController/MotorController.ino
@@ -16,10 +16,10 @@ Servo escs[6];
 Servo directions[2];
 
 //===============================================================================================
-int escPins[6] = {9,10,0,1,2,3};    //Set the pin numbers for motors. Must be PWM. First two have directions. Set to -1 if not connected
-int directionPins[2] = {4,5};           //Set the pin numbers that control the direction of the big motors. Must be PWM
+int escPins[6] = {-1,-1,5,6,-1,-1};    //Set the pin numbers for motors. Must be PWM. First two have directions. Set to -1 if not connected
+int directionPins[2] = {-1,-1};           //Set the pin numbers that control the direction of the big motors. Must be PWM
 //===============================================================================================
-
+int commaIndexes[7];
 int minPulseWidth = 1000;
 int maxPulseWidth = 1100;
 int currentThrottle[6] = {0,0,90,90,90,90};   //The initial throttle of each motor
@@ -51,19 +51,22 @@ void loop() {
   if(Serial.available()) {
     //Read the throttle value and store into variables
     String input = Serial.readString();
-    int commaIndex1 = input.indexOf(',');
-    int commaIndex2 = input.indexOf(',',commaIndex1+1);
-    int motor = (input.substring(0,commaIndex1)).toInt();
-    int throttle = (input.substring(commaIndex1+1,commaIndex2)).toInt();
-    int dir = (input.substring(commaIndex2+1)).toInt();
-
-    //update the arrays
-    if(motor < 2){
-      currentDirections[motor] = dir;
+    currentDirections[0] = input.substring(0,1).toInt();
+    currentThrottle[0] = input.substring(1,4).toInt();
+    currentDirections[1] = input.substring(4,5).toInt();
+    currentThrottle[1] = input.substring(5,8).toInt();
+    currentThrottle[2] = input.substring(8,11).toInt();
+    currentThrottle[3] = input.substring(11,14).toInt();
+    currentThrottle[4] = input.substring(14,17).toInt();
+    currentThrottle[5] = input.substring(17,20).toInt();
+    Serial.println(currentDirections[0]);
+    Serial.println(currentDirections[1]);
+    for(int i = 0; i<6; i++){
+      Serial.println(currentThrottle[i]);
     }
-    currentThrottle[motor] = throttle;
   }
 
+  
   changeThrottle(0);
   changeThrottle(1);
   changeThrottle(2);

31db98effb368428f43f098e370077fc81822b92
Drew Patel <drew1patel@gmail.com>
2019-06-24 12:36:26 -0500

    testing gpiozero

--- Python/NewCodeBase/tesr.py
+++ Python/NewCodeBase/tesr.py
@@ -1,24 +1,7 @@
-import RPi.GPIO as GPIO
-from time import sleep
-
-GPIO.setmode(GPIO.BOARD)
-
-Motor1A = 13
-Motor1B = 18
-Motor1E = 22
-
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor1E, GPIO.OUT)
-
-print("Turning motor on")
-GPIO.output(Motor1A, GPIO.HIGH)
-GPIO.output(Motor1B, GPIO.LOW)
-GPIO.output(Motor1E, GPIO.HIGH)
-
-sleep(2)
-
-print("Stopping motor")
-GPIO.output(Motor1E, GPIO.LOW)
-
-GPIO.cleanup()
+from gpiozero import Motor
+motor1 = Motor(forward=4, backward=14)
+while True:
+    motor1.forward(0.5)
+    sleep(5)
+    motor1.reverse(0.5)
+    sleep(5)

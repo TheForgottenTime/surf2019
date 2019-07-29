871037194934b02925b761b8b2294f0eebb47891
Drew Patel <drew1patel@gmail.com>
2019-07-28 20:51:04 -0700

    removed auto port detection and changed it to RP's port

--- Python/Submarine.py
+++ Python/Submarine.py
@@ -5,18 +5,9 @@ import warnings
 import serial
 import serial.tools.list_ports
 
-arduino_ports = [
-    p.device
-    for p in serial.tools.list_ports.comports()
-    if 'Arduino' in p.description  # may need tweaking to match new arduinos
-]
-if not arduino_ports:
-    raise IOError("No Arduino found")
-if len(arduino_ports) > 1:
-    warnings.warn('Multiple Arduinos found - using the first')
 
 ser = serial.Serial(
-    port=arduino_ports[0],
+    port='/dev/tty/ACMO0',
     baudrate=9600
 )
 
